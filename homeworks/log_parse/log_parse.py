9356c28b342672ba2b1b01cc41944c500d2f4606
AngelaSvoykina <lybovsvoykina@gmail.com>
2018-10-18 23:43:25 +0300

    Log_parse is ready

--- homeworks/log_parse/log_parse.py
+++ homeworks/log_parse/log_parse.py
@@ -1,6 +1,5 @@
 # -*- encoding: utf-8 -*-
 import re
-import numpy
 import math
 import datetime
 from collections import Counter
@@ -28,9 +27,9 @@ def parse(
     extreg = re.compile(r".*\.([a-zA-Z0-9]*)$")
 
     if not ignore_www:
-        reg = r"^\[(.*)] \"([\S]*) ()()([\S]*) ([\S]*)\" ([\S]*) ([\S]*)"
+        reg = r"^\[(.*)] \"([\S]*) (https?:\/\/)()([\S]*) ([\S]*)\" ([\S]*) ([\S]*)"
     else:
-        reg = r"^\[(.*)] \"([\S]*) (https?://)(www\.)?([\S]*) ([\S]*)\" ([\S]*) ([\S]*)"
+        reg = r"^\[(.*)] \"([\S]*) (https?:\/\/)(www\.)?([\S]*) ([\S]*)\" ([\S]*) ([\S]*)"
 
     reg = re.compile(reg)
     for line in (open('log.log')):
@@ -52,43 +51,35 @@ def parse(
             if stop_at < url_date:
                 break
 
-        if len(ignore_urls) > 0:
-            if match.group(5) in ignore_urls:
-                continue
+        if len(ignore_urls) > 0 and match.group(5) in ignore_urls:
+            continue
 
         if ignore_files and re.match(extreg, match.group(5)):
             continue
 
-        # TODO: Всегда добавлять в tuple
-        if request_type is not None:
-            if match.group(2) == request_type:
-                urls_data.insert(match.group(5))
+        if request_type is not None and match.group(2) == request_type:
+            urls_data.append([match.group(5)])
         else:
-            urls_data.insert(match.group(5))
+            urls_data.append([match.group(5)])
 
-        # TODO: Во второй (1-й) элемент tuple записывать
         if slow_queries:
-            if urls_data[1]:
-                urls_data.append(int(match.group(8)))
-
-    # if len(ignore_urls) > 0:
-    #     urls_data = list(filter(lambda x: x not in ignore_urls, urls_data))
+            urls_data[-1].append(int(match.group(8)))
 
     if slow_queries:
-        slow_cnt = Counter(urls_data)
+        slowest = sorted(urls_data, key=lambda x: x[1], reverse=True)[:5]
+        slowest = [[slow_el[0], sum(el[1] for el in urls_data if el[0] == slow_el[0])] for slow_el in slowest]
+        urls_cnt = Counter(el[0] for el in urls_data)
 
-        #print(sorted(urls_data))
-
-        #result = math.floor(numpy.mean(slow_cnt.most_common()[:-5:-1]))
+        slowest = [math.floor(el[1]/urls_cnt[el[0]]) for el in slowest]
+        result = sorted(slowest, reverse=True)
     else:
-        urls_cnt = Counter(urls_data)
+        urls_cnt = Counter(el[0] for el in urls_data)
 
         urls_list = urls_cnt.most_common(5)
         urls_list = [el[1] for el in urls_list]
 
         result = urls_list
 
-    print(result)
     return result
 
 
