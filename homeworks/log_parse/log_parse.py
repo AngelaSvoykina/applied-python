0709856cc7977b17a07076440cd2dd3d81ce5b37
AngelaSvoykina <lybovsvoykina@gmail.com>
2018-10-19 17:23:17 +0300

    I change stop and start

--- homeworks/log_parse/log_parse.py
+++ homeworks/log_parse/log_parse.py
@@ -16,12 +16,6 @@ def parse(
 ):
     urls_data = []
 
-    if start_at:
-        start_at = datetime.datetime.strptime(start_at, '%d/%b/%Y %H:%M:%S')
-
-    if stop_at:
-        stop_at = datetime.datetime.strptime(stop_at, '%d/%b/%Y %H:%M:%S')
-
     result = []
 
     extreg = re.compile(r".*\.([a-zA-Z0-9]*)$")
@@ -65,7 +59,7 @@ def parse(
             urls_data[-1].append(int(match.group(8)))
 
     if slow_queries:
-        slowest = sorted(urls_data, key=lambda x: x[1], reverse=True)[:5]
+        slowest = sorted(urls_data, key=lambda x: x[1], reverse=True)[:5] #топ 5 самых медленных
         slowest = [[slow_el[0], sum(el[1] for el in urls_data if el[0] == slow_el[0])] for slow_el in slowest]
         urls_cnt = Counter(el[0] for el in urls_data)
 
@@ -83,7 +77,7 @@ def parse(
 
 
 def main():
-    parse(slow_queries=True)
+    parse()
 
 
 if __name__ == '__main__':
