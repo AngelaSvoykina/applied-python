a405beb4c4e6685a15a62c8b0a6b7b62e57fc8d9
AngelaSvoykina <lybovsvoykina@gmail.com>
2018-11-30 14:50:08 +0300

    Изменения в схеме

--- homeworks/blog/db/schema.sql
+++ homeworks/blog/db/schema.sql
@@ -3,45 +3,39 @@ CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(150) UNIQUE NOT NULL,
   f_name   VARCHAR(255)        NOT NULL,
   l_name   VARCHAR(255)        NOT NULL,
-  password VARCHAR(255)           NOT NULL
+  password VARCHAR(255)        NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS sess (
   id      SERIAL PRIMARY KEY,
-  user_id INT       NOT NULL,
+  user_id INT       NOT NULL REFERENCES users (id) ON DELETE SET NULL,
   sess    CHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS blogs (
   id            SERIAL PRIMARY KEY,
   title         VARCHAR(255) NOT NULL,
-  blog_posts_id INT          NOT NULL
+  author_id INT NOT NULL REFERENCES users (id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS blog_posts (
-  id        SERIAL PRIMARY KEY,
-  author_id INT NOT NULL UNIQUE
+  post_id    INT     NOT NULL REFERENCES posts (id) ON DELETE SET NULL,
+  blog_id    INT     NOT NULL REFERENCES blogs (id) ON DELETE SET NULL
 );
-
 
 CREATE TABLE IF NOT EXISTS posts (
   id            SERIAL PRIMARY KEY,
   title         VARCHAR(255) NOT NULL,
   text          VARCHAR(255) NOT NULL,
   created       TIMESTAMP DEFAULT now(),
-  blog_posts_id INT          NOT NULL
+  author_id INT NOT NULL REFERENCES users (id) ON DELETE SET NULL
 );
-
 
 CREATE TABLE IF NOT EXISTS comments (
   id         SERIAL PRIMARY KEY,
   text       VARCHAR(255) NOT NULL,
   created    TIMESTAMP DEFAULT now(),
-  comment_id INT,
-  post_id    INT          NOT NULL
+  comment_id INT REFERENCES comments (id),
+  post_id    INT          NOT NULL REFERENCES posts (id) ON DELETE SET NULL,
+  author_id INT NOT NULL REFERENCES users (id)
 );
-
-CREATE TABLE IF NOT EXISTS blogipost (
-  post_id    INT     NOT NULL,
-  blog_id    INT     NOT NULL
-)
