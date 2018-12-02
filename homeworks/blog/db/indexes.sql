d40c1d7f20160a4c840e8f3e86159e2adb73bbbd
AngelaSvoykina <lybovsvoykina@gmail.com>
2018-12-02 21:32:05 +0300

    Последняя версия

--- homeworks/blog/db/indexes.sql
+++ homeworks/blog/db/indexes.sql
@@ -1,29 +1,11 @@
-CREATE INDEX users_id_index
-  ON users (id);
+CREATE INDEX users_username_index ON users (username);
+CREATE INDEX users_pass_index ON users (password);
 
-CREATE INDEX sess_id_index
-  ON sess (id);
+CREATE INDEX sess_user_id_index ON sess (user_id);
+CREATE INDEX sess_index ON sess (sess);
 
-CREATE INDEX sess_user_id_index
-  ON sess (user_id);
+CREATE INDEX blogs_thread_id_index ON blogs (author_id);
+CREATE INDEX blog_posts_id_index ON blog_posts (post_id);
 
-CREATE INDEX blogs_id_index
-  ON blogs (id);
-
-CREATE INDEX blogs_thread_id_index
-  ON blogs (blog_posts_id);
-
-CREATE INDEX blog_posts_id_index
-  ON blog_posts (id);
-CREATE INDEX blog_posts_author_id_index
-  ON blog_posts (author_id);
-
-CREATE INDEX posts_id_index
-  ON posts (id);
-CREATE INDEX posts_blog_posts_id_index
-  ON posts (blog_posts_id);
-
-CREATE INDEX comments_id_index
-  ON comments (id);
-CREATE INDEX comments_post_id_index
-  ON comments (post_id);
+CREATE INDEX comments_post_id_index ON comments (post_id);
+CREATE INDEX comments_auth_id_index ON comments (author_id);
