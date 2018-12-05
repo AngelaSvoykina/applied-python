bb3ea8be67f552b3d8eefe56873d8f5b16de0822
AngelaSvoykina <lybovsvoykina@gmail.com>
2018-12-05 14:43:31 +0300

    изменены индексы

--- homeworks/blog/db/indexes.sql
+++ homeworks/blog/db/indexes.sql
@@ -1,11 +1,13 @@
 CREATE INDEX users_username_index ON users (username);
-CREATE INDEX users_pass_index ON users (password);
+CREATE INDEX users_username_password_index ON users (username, password);
 
 CREATE INDEX sess_user_id_index ON sess (user_id);
 CREATE INDEX sess_index ON sess (sess);
 
 CREATE INDEX blogs_thread_id_index ON blogs (author_id);
+
 CREATE INDEX blog_posts_id_index ON blog_posts (post_id);
+CREATE INDEX blog_posts_id_index ON blog_posts (blog_id);
 
 CREATE INDEX comments_post_id_index ON comments (post_id);
 CREATE INDEX comments_auth_id_index ON comments (author_id);
